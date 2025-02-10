b5ecb1f3e6960acae4040b30e7c2232ded7a1ddb
Gott95 <51401433+Gott95@users.noreply.github.com>
2025-02-09 22:34:28 -0600

    working

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 
+function agregarAmigo() {
+  let nombres = document.getElementById("amigo").value;
 
-function agregarAmigo () {
-     let nombres = document.getElementById('amigo').value;
+  if (nombres == "") {
+    alert("Inserte algun nombre");
+    return;
+  }
+  if (amigos.includes(nombres)) {
+    alert("El nombre ya existe");
+    return;
+  } else {
+    amigos.push(nombres);
+    document.getElementById("amigo").value = "";
+    mostrarAmigos();
+  }
+}
 
-     if (nombres == '') {
-        alert('Inserte algun nombre')
+function mostrarAmigos() {
+  let listaAmigos = document.getElementById("listaAmigos");
+  listaAmigos.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    listaAmigos.innerHTML += `<li>${amigos[i]}</li>`;
+  }
+}
 
-
-     } else {
-
-        amigos.push(nombres);
-
-        document.getElementById('amigo').value = '';
-
-        document.getElementById('listaAmigos').innerHTML = 'amigos';
-
-        console.log(amigos);
-     }
-
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("No hay amigos para sortear");
+    return;
+  } else {
+    let amigoSorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    document.getElementById(  "resultado"   ).innerHTML = `El amigo sorteado es ${amigoSorteado}`;
+  }
 }
